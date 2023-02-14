0c166289a2a2d68f8e45c6a81eb1eeafed1a808c
TheuZCoder <matheusrdsilva1@gmail.com>
2023-02-14 17:06:47 -0200

    14/02/23

--- Java/Viagem/CalcularDistancia.java
+++ Java/Viagem/CalcularDistancia.java
@@ -3,21 +3,23 @@ package Viagem;
 import java.util.Scanner;
 
 public class CalcularDistancia {
-    Scanner sc = new Scanner(System.in);
-    String Saida,Destino;
+      Scanner sc = new Scanner(System.in);
+        String Saida, Destino;
 
-    public void Destino(){
+    public void Destino() {
 
         System.out.println("Informe seu local de saida: ");
         Saida = sc.nextLine();
         System.out.println("Informe o Destino: ");
         Destino = sc.nextLine();
         System.out.println("você viajara de " + Saida + " até " + Destino);
-    }
-    public void Distancia(){
 
-        try (Scanner ler = new Scanner(System.in)){
-        double distanciaPercorrida,preçoDaGasolina,KMporLitro,custoViagem;  //utilizar sempre o double para usar numeros quebrados no codigo sem dar crash em vez do int
+       }    
+    
+    public void Distancia() {
+
+        try (Scanner ler = new Scanner(System.in)) {
+            double distanciaPercorrida, preçoDaGasolina, KMporLitro, custoViagem;
 
             System.out.println("=============Calculador de preços de viagem===========");
             System.out.println("Qual a distancia percorrida?: ");
@@ -27,10 +29,11 @@ public class CalcularDistancia {
             System.out.println("Quantos KM por litro faz seu carro?: ");
             KMporLitro = ler.nextDouble();
             custoViagem = distanciaPercorrida / KMporLitro * preçoDaGasolina;
-    
-   System.out.println("O custo da sua viagem foi de " + custoViagem + " Reais" );
-    System.out.println("=================Programa finalizado===============");
+            ler.close();
+
+            System.out.println("O custo da sua viagem foi de " + custoViagem + " Reais");
+            System.out.println("=================Programa finalizado===============");
         }
     }
-    
-}   
+
+}
---
+++ Operadores/app.java
@@ -0,0 +1,9 @@
+package Operadores;
+
+public class app {
+    public static void main(String[] args) {
+       
+        Operadores objeto1 = new Operadores();
+        objeto1.aritmetico();
+    }
+}
