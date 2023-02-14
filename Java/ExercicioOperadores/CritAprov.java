7e0d1633a96be539e548e0f1a817cda1029f1a02
TheuZCoder <matheusrdsilva1@gmail.com>
2023-02-14 18:07:58 -0200

    14/02/23

---
+++ Java/ExercicioOperadores/CritAprov.java
@@ -0,0 +1,20 @@
+package ExercicioOperadores;
+import java.util.Scanner;
+
+public class CritAprov {
+    Scanner ler = new Scanner(System.in);
+    double NotaUm,NotaDois,NotaFinal;
+
+    public void aprovacao(){
+        System.out.println("Informar Nota um aulo: ");
+        NotaUm = ler.nextDouble();
+        System.out.println("informar segunda nota: ");
+        NotaDois = ler.nextDouble();
+        NotaFinal = NotaUm + NotaDois / 2;
+       
+
+
+
+        
+    }
+}
