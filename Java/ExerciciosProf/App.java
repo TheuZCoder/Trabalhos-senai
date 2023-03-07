1643bb9f09693cbed47913db602c0164884df4c8
TheuZCoder <matheusrdsilva1@gmail.com>
2023-03-07 13:47:24 -0300

    070323

--- Java/ExerciciosProf/App.java
+++ Java/ExerciciosProf/App.java
@@ -2,17 +2,13 @@ package ExerciciosProf;
 
 public class App {
     public static void main(String[] args) {
-        Exercicio1 objeto1 = new Exercicio1();
-        objeto1.ValorMaior();
-        Exercicio2 objeto2 = new Exercicio2();
-        objeto2.VerificaVoto();
-        Exercicio3 objeto3 = new Exercicio3();
-        objeto3.VerificaSenha();
-        Exercicio4 objeto4 = new Exercicio4();
-        objeto4.VerificaSenha();
-        Exercicio5 objeto5 = new Exercicio5();
-        objeto5.VerificaSenha();
-        Exercicio6 objeto6 = new Exercicio6();
-        objeto6.VerificaSenha();
+        Exercicios objeto1 = new Exercicios();
+        objeto1.exe1();
+        objeto1.exe2();
+        objeto1.exe3();
+        objeto1.exe4();
+        objeto1.exe5();
+        objeto1.exe6();
     }
 }
+
--- Java/ExerciciosProf/Exercicio4.java
+++
@@ -1,6 +0,0 @@
-package ExerciciosProf;
-import java.util.Scanner;
-
-public class Exercicio4 {
-    Scanner leitura = new Scanner(System.in);
-    
