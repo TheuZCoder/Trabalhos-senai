12df167cf6a22213c679e4ad14373dbf7f2bf67a
TheuZCoder <matheusrdsilva1@gmail.com>
2023-03-14 17:07:46 -0300

    14/03/23 16:58

---
+++ Java/ExerciciosVetores/Exercicio1.java
@@ -0,0 +1,26 @@
+package ExerciciosVetores;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+    Scanner leitura = new Scanner(System.in);
+    double valores[];
+
+    public void vetor1(){
+        valores = new double[]{1,2,3,4,5,6,7,8,9,10};
+        System.out.println("o vetor (1) invertido fica = " + valores[9]);
+        System.out.println("o vetor (2) invertido fica = " + valores[8]);
+        System.out.println("o vetor (3) invertido fica = " + valores[7]);
+        System.out.println("o vetor (4) invertido fica = " + valores[6]);
+        System.out.println("o vetor (5) invertido fica = " + valores[5]);
+        System.out.println("o vetor (6) invertido fica = " + valores[4]);
+        System.out.println("o vetor (7) invertido fica = " + valores[3]);
+        System.out.println("o vetor (8) invertido fica = " + valores[2]);
+        System.out.println("o vetor (9) invertido fica = " + valores[1]);
+        System.out.println("o vetor (10) invertido fica = " + valores[0]);
+
+
+        
+        
+    }
+}
