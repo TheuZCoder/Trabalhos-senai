29642e74a1398aa42a401b3ad68935788692dbb4
Theuz <82974688+TheuZCoder@users.noreply.github.com>
2023-04-18 17:59:30 +0000

    18/04 15:00

---
+++ Java/iteração/app.java
@@ -0,0 +1,11 @@
+package iteração;
+
+/**
+ * app
+ */
+public class app {
+    public static void main(String[] args) {
+        it obj = new it();
+        obj.ex2();
+    }
+}
---
+++ Java/iteração/it.java
@@ -0,0 +1,71 @@
+package iteração;
+
+import java.util.Scanner;
+
+/**
+ * it
+ */
+public class it {
+    public void ex1() {
+        Scanner leitura = new Scanner(System.in);
+        char[] vetor = new char[10];
+        int numConsoantes = 0;
+        String consoantes = "";
+
+        System.out.println("Digite 10 caracteres:");
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = leitura.next().charAt(0);
+            if (Character.isLetter(vetor[i])) {
+                char c = Character.toLowerCase(vetor[i]);
+                if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u') {
+                    numConsoantes++;
+                    consoantes += vetor[i] + " ";
+                }
+            }
+        }
+
+        System.out.println("Número de consoantes: " + numConsoantes);
+        System.out.println("Consoantes lidas: " + consoantes);
+
+        leitura.close();
+    }
+
+    public void ex2() {
+        Scanner leitura = new Scanner(System.in);
+
+        int[] numeros = new int[20];
+        int[] pares = new int[20];
+        int[] impares = new int[20];
+        int numPares = 0, numImpares = 0;
+
+        System.out.println("Digite 20 números inteiros:");
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = leitura.nextInt();
+            if (numeros[i] % 2 == 0) {
+                pares[numPares++] = numeros[i];
+            } else {
+                impares[numImpares++] = numeros[i];
+            }
+        }
+
+        System.out.print("Números digitados: ");
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.print(numeros[i] + " ");
+        }
+        System.out.println();
+
+        System.out.print("Números pares: ");
+        for (int i = 0; i < numPares; i++) {
+            System.out.print(pares[i] + " ");
+        }
+        System.out.println();
+
+        System.out.print("Números ímpares: ");
+        for (int i = 0; i < numImpares; i++) {
+            System.out.print(impares[i] + " ");
+        }
+        System.out.println();
+
+        leitura.close();
+    }
+}   
