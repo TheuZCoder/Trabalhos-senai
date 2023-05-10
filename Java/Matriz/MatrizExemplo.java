5b0db11be354f77cfc0b7a8c9878c6c122aba50e
Theuz <82974688+TheuZCoder@users.noreply.github.com>
2023-05-10 13:43:34 +0000

    10/05

--- Java/Matriz/MatrizExemplo.java
+++ Java/Matriz/MatrizExemplo.java
@@ -133,6 +133,29 @@ public class MatrizExemplo {
             System.out.println("Valor não enconttrado");
         }
     }
+    public void Exercicio4Extra() {
+        int A[][] = new int[10][10];
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (i < j) {
+                    A[i][j] = 2 * i + 7 * j - 2;
+                } else if (i == j) {
+                    A[i][j] = 3 * i * i - 1;
+                } else {
+                    A[i][j] = 4 * i * i * i - 5 * j * j + 1;
+                }
+
+            }
+        }
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A.length; j++) {
+                System.out.println(A[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+    }
 
     public void Exercicio5Extra() {
         int matriz[][] = new int[5][4], NotaMaior = matriz[0][3], MatriculaMaior = matriz[0][0], SomaNotas = 0;
