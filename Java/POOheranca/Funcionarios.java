58c9a5b14df9228b503f9644af86389dfa865a91
Theuz <82974688+TheuZCoder@users.noreply.github.com>
2023-05-16 19:28:19 +0000

    16/05

--- Java/POOheranca/Funcionarios.java
+++ Java/POOheranca/Funcionarios.java
@@ -3,4 +3,19 @@ package POOheranca;
 public class Funcionarios extends Pessoas {
     String seguranca;
     String faxineiros;
+
+    
+    public String getSeguranca() {
+        return seguranca;
+    }
+    public void setSeguranca(String seguranca) {
+        this.seguranca = seguranca;
+    }
+    public String getFaxineiros() {
+        return faxineiros;
+    }
+    public void setFaxineiros(String faxineiros) {
+        this.faxineiros = faxineiros;
+    }
+    
 }
--- Java/POOheranca/professores.java
+++ Java/POOheranca/professores.java
@@ -3,5 +3,19 @@ package POOheranca;
 public class professores extends Pessoas{
     String materias;
     String materiais;
+
+    
+    public String getMaterias() {
+        return materias;
+    }
+    public void setMaterias(String materias) {
+        this.materias = materias;
+    }
+    public String getMateriais() {
+        return materiais;
+    }
+    public void setMateriais(String materiais) {
+        this.materiais = materiais;
+    }
     
 }
