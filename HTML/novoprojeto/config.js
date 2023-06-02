73a3c051ef2bce192ec7531c1e749648be2b1f00
Theuz <82974688+TheuZCoder@users.noreply.github.com>
2023-06-02 19:13:13 +0000

    02/06

---
+++ HTML/novoprojeto/config.js
@@ -0,0 +1,8 @@
+document.getElementById("loginBtn").addEventListener("click", function() {
+    document.getElementById("loginModal").style.display = "block";
+  });
+  
+  document.getElementById("closeBtn").addEventListener("click", function() {
+    document.getElementById("loginModal").style.display = "none";
+  });
+  
