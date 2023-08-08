de7905173e9252cb44555aac64f28bf35c9f0eb8
TheuZCoder <matheusrdsilva1@gmail.com>
2023-08-08 16:42:20 -0300

    att

--- Java/exception/senha.java
+++ Java/exception/senha.java
@@ -5,24 +5,29 @@ import javax.swing.JOptionPane;
 public class senha {
 
     public static void main(String[] args) {
-        String senha = JOptionPane.showInputDialog("Digite a senha:");
-
-        if (senha.length() < 8) {
-            JOptionPane.showMessageDialog(null, "A senha deve conter pelo menos 8 caracteres.");
-            return;
+        //
+        try {
+            JOptionPane.showMessageDialog(null, "Welcome to SENAI");
+            String login = JOptionPane.showInputDialog("Informe seu Login");
+            String dataNascimento = JOptionPane.showInputDialog("Informe sua Data de Nascimento [dd/mm/aa]");
+            dataNascimento=dataNascimento.replace("/", "");
+            System.out.println(dataNascimento);
+            String senha = JOptionPane.showInputDialog("Informe sua Senha de 6 Digitos");
+            
+            if (senha.length() != 6) {
+                throw new Exception("Senha deve ter 6 Digitos");
+            }
+            if(senha.equals(login)){
+                throw new Exception("Senha = login");
+            }
+            if(!senha.matches(".*[a-z].*") || !senha.matches(".*[A-Z].*") ){
+                throw new Exception("falta Letra Lower and Upper");
+            }
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+            JOptionPane.showMessageDialog(null, e.getMessage(),"senha",0);
         }
-
-        if (!senha.matches(".*[a-z].*")) {
-            JOptionPane.showMessageDialog(null, "A senha deve conter pelo menos uma letra minúscula.");
-            return;
-        }
-
-        if (!senha.matches(".*[A-Z].*")) {
-            JOptionPane.showMessageDialog(null, "A senha deve conter pelo menos uma letra maiúscula.");
-            return;
-        }
-
-        JOptionPane.showMessageDialog(null, "Senha válida!");
     }
 }
 
