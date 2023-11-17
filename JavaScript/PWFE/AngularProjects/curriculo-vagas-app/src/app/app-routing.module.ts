69181f4304a7ee7149b7edecb65e303695f20f8b
TheuZCoder <matheusrdsilva1@gmail.com>
2023-11-17 15:53:45 -0300

    atualizado header com curriculos

--- JavaScript/PWFE/AngularProjects/curriculo-vagas-app/src/app/app-routing.module.ts
+++ JavaScript/PWFE/AngularProjects/curriculo-vagas-app/src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { InicioComponent } from './view/inicio/inicio.component';
 import { CurriculosComponent } from './view/curriculos/curriculos.component';
 import { PainelVagasComponent } from './view/painel-vagas/painel-vagas.component';
 import { VagasComponent } from './view/vagas/vagas.component';
+import { PainelCurriculosComponent } from './view/painel-curriculos/painel-curriculos.component';
 
 const routes: Routes = [
   {path: '', component: InicioComponent},
   {path: 'curriculos', component: CurriculosComponent},
+  {path: 'painelCurriculos', component: PainelCurriculosComponent},
   {path: 'vagas', component: VagasComponent},
   {path: 'painel', component: PainelVagasComponent}
 ];
